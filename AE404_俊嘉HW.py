dcdff0292e646cfc05b67c7b43675c6a0d9c52d5
Herbie Chu <65217537+herbiecool@users.noreply.github.com>
2021-07-07 21:42:56 +0800

    Add files via upload

---
+++ AE404_俊嘉HW.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import requests
+
+r = requests.get('https://tw.yahoo.com/') 
+
+print(r)
+print(r.text)
+
+
+
+
+
+
+
